d8903214dfa7f40f144b2a34395d7caab176a4a9
Houssem Nasri <housi.housi2015@gmail.com>
2022-11-21 02:46:13 +0100

    Define Graphic disposal methods

---
+++ src/main/java/tech/houssemnasri/gifx/GraphicDisposalMethod.java
@@ -0,0 +1,23 @@
+package tech.houssemnasri.gifx;
+
+import java.util.Arrays;
+
+public enum GraphicDisposalMethod {
+    NO_DISPOSAL_SPECIFIED(0),
+    DO_NOT_DISPOSE(1),
+    RESTORE_TO_BACKGROUND_COLOR(2),
+    RESTORE_YO_PREVIOUS(3);
+
+    private final int value;
+
+    GraphicDisposalMethod(int value) {
+        this.value = value;
+    }
+
+    public GraphicDisposalMethod parse(int disposalMethodValue) {
+        return Arrays.stream(values())
+                     .filter(disposalMethod -> disposalMethod.value == disposalMethodValue)
+                     .findAny()
+                     .orElseThrow(() -> new IllegalArgumentException("Invalid disposal method: " + disposalMethodValue));
+    }
+}
