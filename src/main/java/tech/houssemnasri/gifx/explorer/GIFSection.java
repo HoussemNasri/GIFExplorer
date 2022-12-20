06d7a577248b1cf24d39d1c8c7a599e78deff2e5
Houssem Nasri <housi.housi2015@gmail.com>
2022-12-20 06:14:53 +0100

    Take section bytes as an argument in GIFSection

--- src/main/java/tech/houssemnasri/gifx/explorer/GIFSection.java
+++ src/main/java/tech/houssemnasri/gifx/explorer/GIFSection.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
+import javafx.scene.Node;
 
 public class GIFSection {
     private final String title;
@@ -13,14 +14,21 @@ public class GIFSection {
     private final int length;
     private final Color color;
     private final Map<String, String> properties = new HashMap<>();
-    private GIFSectionPreview sectionPreview;
 
-    public GIFSection(String title, int offset, int length, Map<String, String> properties, Color color) {
+    private final Integer[] bytes;
+    private javafx.scene.Node sectionPreview;
+
+    public GIFSection(String title, int offset, int length, Map<String, String> properties, Color color, Integer[] bytes) {
         this.title = title;
         this.offset = offset;
         this.length = length;
         this.color = color;
+        this.bytes = bytes;
         this.properties.putAll(properties);
+    }
+
+    public Integer[] getBytes() {
+        return bytes;
     }
 
     public String getTitle() {
@@ -35,12 +43,16 @@ public class GIFSection {
         return length;
     }
 
-    public Optional<GIFSectionPreview> getSectionPreview() {
+    public void setPreview(Node preview) {
+        this.sectionPreview = preview;
+    }
+
+    public Optional<javafx.scene.Node> getPreview() {
         return Optional.ofNullable(sectionPreview);
     }
 
     public boolean hasPreview() {
-        return getSectionPreview().isPresent();
+        return getPreview().isPresent();
     }
 
     public Map<String, String> getProperties() {
